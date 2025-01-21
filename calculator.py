3920ef5524183cb56c72a1a7a2ea2a2e14c772b5
Alexander A <alexanderstevenarcia@gmail.com>
2025-01-21 12:07:39 -0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,29 @@
+num1 = 5
+num2 = 6
+result = num1 * num2
+print(result)
+
+num1 = 8
+num2 = 3
+result = num1 / num2
+print(result)
+
+num1 = 8
+num2 = 3
+result = num1 // num2
+print(result)
+
+result = 8 % 3
+print(result)
+
+num1 = 4200000
+num2 = 23090
+result = num1 + num2
+print(result)
+
+num1 = 4200000
+num2 = 7633
+result = num1 - num2
+print(result)
+
+print("Alexander Arcia")
